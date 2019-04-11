4ffdca34f89c2631ec44afb45d8f95b36e074798
mcerruti0311 <mcerruti@sis.us>
2019-04-10 17:05:15 -0700

    190410

--- classes/itemsToMatchBase.sql
+++ classes/itemsToMatchBase.sql
@@ -1,6 +1,8 @@
 SELECT Id,
     Name,
     Uniform_Type__c,
+    Manufacturer_Picklist__c,
+    Manufacturer_xxx__c,
     Description__c,
     Manufacturer_Stock_Number_Picklist__c,
     IMS_Detail__c,
@@ -8,16 +10,14 @@ SELECT Id,
     Size__c,
     Waist__c,
     Length__c
+
 FROM Uniform_Stock_Database__c
 WHERE Retired_Item__c = false
-    AND (NOT Location_Data__r.Name LIKE '%github%')
-    AND Manufacturer_Picklist__c = '5.11'
-    AND Manufacturer_Stock_Number_Picklist__c = ''
+    AND Uniform_Currently_Issued__c = false
     AND Uniform_Type__c = 'Pants'
-    AND Size__c = ''
-    AND Waist__c != ''
+    AND Manufacturer_Picklist__c = 'LawPro'
+    AND Manufacturer_Stock_Number_Picklist__c = 'TU176'
     AND Length__c != ''
+    AND Waist__c != ''
     AND IMS_Detail__c = ''
-    AND Manufacturer_Stock_Number_Picklist__c = ''
-    AND (Description__c LIKE '%')
-ORDER BY Waist__c, Length__c
+ORDER BY Size__c, Waist__c, Length__c
