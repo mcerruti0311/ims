d2ca24dc44903c1ed6a452c5401702b318c0bacf
mcerruti0311 <mcerruti@sis.us>
2019-04-16 06:29:39 -0700

    Morning Push

--- classes/poloToMatch.sql
+++ classes/poloToMatch.sql
@@ -7,14 +7,19 @@ SELECT Id,
   Manufacturer_Stock_Number_Picklist__c,
   IMS_Detail__c,
   IMS_Detail__r.Item_Description__c,
-  Size__c,
+  Size__c
 
 FROM Uniform_Stock_Database__c
-WHERE Retired_Item__c = false
-  AND IMS_Detail__c = ''
-  AND Uniform_Type__c = 'Polo'
-  AND Manufacturer_Picklist__c != ''
-  AND Manufacturer_Stock_Number_Picklist__c != ''
-  AND Size__c != ''
+WHERE Retired_Item__c
+= false
+AND IMS_Detail__c = ''
+    AND Uniform_Type__c = 'Polo'
+    AND Size__c != '' 
+    AND Manufacturer_Picklist__c LIKE 'Port Authority'
+    AND
+(Manufacturer_Stock_Number_Picklist__c = '')
+    AND Description__c LIKE '%black%long%beats%'
+
+ORDER BY Size__c
 
 ORDER BY Size__c
