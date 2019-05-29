08a309f506ba108546d18f21422cdef2ab538c5a
mcerruti0311 <mcerruti@sis.us>
2019-05-29 06:01:46 -0700

    20190529AM

--- classes/polos/poloToMatch.sql
+++ classes/polos/poloToMatch.sql
@@ -2,21 +2,22 @@ SELECT Id,
   Name,
   Uniform_Type__c,
   Manufacturer_Picklist__c,
-  Manufacturer_xxx__c,
   Description__c,
   Manufacturer_Stock_Number_Picklist__c,
   IMS_Detail__c,
   IMS_Detail__r.Item_Description__c,
   Size__c,
-  Location_Data__r.Name
+  Waist__c,
+  Length__c,
+  Location_Data__r.Name,
+  LastModifiedDate,
+  LastModifiedBy.Name
 
 FROM Uniform_Stock_Database__c
 WHERE Retired_Item__c = false
-AND IMS_Detail__c = ''
-    AND Uniform_Type__c = 'Polo'
-    AND Manufacturer_Picklist__c != ''
-    AND Manufacturer_Stock_Number_Picklist__c != ''
-    AND Description__c LIKE '%'
-    AND Size__c != ''    
-
+  AND IMS_Detail__c = ''
+  AND Uniform_Type__c = 'Polo'
+  AND Manufacturer_Picklist__c = ''
+  AND Manufacturer_Stock_Number_Picklist__c = ''
+  AND Description__c LIKE '%%'
 ORDER BY Size__c
