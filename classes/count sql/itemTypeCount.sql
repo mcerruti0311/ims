34751c86a219d5601f530ac7ba4174cb9bac38e2
mcerruti0311 <mcerruti@sis.us>
2019-06-24 05:43:27 -0700

    today

--- classes/count sql/itemTypeCount.sql
+++ classes/count sql/itemTypeCount.sql
@@ -4,4 +4,6 @@ WHERE Retired_Item__c = false
     AND IMS_Detail__c = ''
     AND Uniform_Type__c != ''
     AND (Size__c != '' OR Waist__c != '' OR Length__c != '')
+    AND Manufacturer_Picklist__c != ''
+    AND Manufacturer_Stock_Number_Picklist__c != ''
 GROUP BY Uniform_Type__c
--- classes/polos/poloToMatch.sql
+++ classes/polos/poloToMatch.sql
@@ -17,7 +17,7 @@ FROM Uniform_Stock_Database__c
 WHERE Retired_Item__c = false
   AND IMS_Detail__c = ''
   AND Uniform_Type__c = 'Polo'
-  AND Manufacturer_Picklist__c = ''
-  AND Manufacturer_Stock_Number_Picklist__c = ''
+  AND Manufacturer_Picklist__c != ''
+  AND Manufacturer_Stock_Number_Picklist__c != ''
   AND Description__c LIKE '%%'
 ORDER BY Size__c
